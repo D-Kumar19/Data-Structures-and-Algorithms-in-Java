074dbb57009509cfd70417dc7bac0b14824daeab
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2022-06-29 14:42:38 +0200

    Water Bill Challenge

--- src/main/java/CodingChallenges/CalculateWaterBill.java
+++ src/main/java/CodingChallenges/CalculateWaterBill.java
@@ -13,6 +13,7 @@ public class CalculateWaterBill {
             System.out.print("Enter the number of water liters consumed: ");
             waterConsumed = input.nextDouble();
 
+            if(waterConsumed < 0) throw new InputMismatchException();
             System.out.println("Your bill is: " + calculateWaterBill(waterConsumed));
         } catch (InputMismatchException e) {
             System.out.println("Sorry! This is not a Valid Input!");
@@ -20,7 +21,11 @@ public class CalculateWaterBill {
     }
     public static double calculateWaterBill(double waterConsumed){
         final int oneCCF = 748;
+        final double oneCCFBill = 3.90;
+        final double initialAmount = 18.84;
+        if(waterConsumed <= 1496) return initialAmount;
         int numberOfCCFs = (int)Math.ceil(waterConsumed/oneCCF);
-        return 18.84 + ((numberOfCCFs - 2) * 3.90);
+        double extraAmount = (numberOfCCFs - 2) * oneCCFBill;
+        return 18.84 + extraAmount;
     }
 }
