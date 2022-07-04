08ddaa972535039c760b42553239618bbef0c883
Dheeraj Kumar <dheerajkumar.190102@gmail.com>
2022-07-04 11:22:42 +0200

    Interface Example using a Gaming Console

---
+++ src/main/java/OOP/UsingClasses/InterfaceExampleGamingConsole/Chess.java
@@ -0,0 +1,23 @@
+package OOP.UsingClasses.InterfaceExampleGamingConsole;
+
+public class Chess implements GamingConsole {
+    @Override
+    public void up() {
+        System.out.println("Moving Piece Up!");
+    }
+
+    @Override
+    public void down() {
+        System.out.println("Moving Piece Down!");
+    }
+
+    @Override
+    public void left() {
+        System.out.println("Moving Piece Left!");
+    }
+
+    @Override
+    public void right() {
+        System.out.println("Moving Piece Right!");
+    }
+}
---
+++ src/main/java/OOP/UsingClasses/InterfaceExampleGamingConsole/GamingConsole.java
@@ -0,0 +1,11 @@
+package OOP.UsingClasses.InterfaceExampleGamingConsole;
+
+public interface GamingConsole {
+    void up();
+
+    void down();
+
+    void left();
+
+    void right();
+}
